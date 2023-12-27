c9aac5da140362981b169832c9565872019c1a1b
unknown <admin@PNVL068.pn.com.vn>
2023-12-27 22:39:22 +0700

    first commit

---
+++ models/post.model.php
@@ -0,0 +1,9 @@
+<?php 
+    function getAllPost(){
+        global $connection;
+        $statement = $connection->prepare("select * from posts");
+        $statement->execute();
+        $posts = $statement->fetchAll();    
+        return $posts;
+    }
+?>
